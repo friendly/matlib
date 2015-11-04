fee558e22b829fbc46c2f271eb21d4f92c12e4d7
Michael Friendly <friendly@yorku.ca>
2015-11-04 10:21:43 -0500

    modified plotEqn, add URL to DESCRIPTION

--- R/plotEqn.R
+++ R/plotEqn.R
@@ -11,12 +11,17 @@
 #'        If supplied, the length must be equal to the number of unknowns in the equations.
 #'        The default is \code{paste0("x", 1:ncol(A)}.
 #' @param xlim horizontal axis limits for the first variable
-#' @param ylim horizontal axis limits for the second variable
+#' @param ylim horizontal axis limits for the second variable; if missing, \code{ylim} is calculated from the
+#'        range of the set of equations over the \code{xlim}.
 #' @param col scalar or vector of colors for the lines, recycled as necessary
 #' @param lwd scalar or vector of line widths for the lines, recycled as necessary
 #' @param lty scalar or vector of line types for the lines, recycled as necessary
 #' @param axes logical; draw horizontal and vertical axes through (0,0)?
-#' @param labels logical, or a vector of character labels for the equations
+#' @param labels logical, or a vector of character labels for the equations; if \code{TRUE}, each equation is labeled
+#'      using the character string resulting from \code{\link{showEqn}}
+#' @param solution logical; should the solution points for pairs of equations be marked?
+#' @author Michael Friendly
+#' @seealso \code{\link{showEqn}}
 #' @examples
 #' # consistent equations
 #' A<- matrix(c(1,2,3, -1, 2, 1),3,2)
@@ -32,7 +37,9 @@
 
 plotEqn <- function(A, b, vars, xlim=c(-4, 4), ylim,
 				col=1:nrow(A), lwd=2, lty=1,
-				axes=TRUE, labels=TRUE) {
+				axes=TRUE, labels=TRUE,
+				solution=TRUE
+				) {
   if (!is.numeric(A) || !is.matrix(A)) stop("A must be a numeric matrix")
   if (missing(b)) {
     b <- A[,ncol(A)]   # assume last column of Ab
@@ -64,8 +71,17 @@ plotEqn <- function(A, b, vars, xlim=c(-4, 4), ylim,
 	}
 	else labels=NULL
 
-		for (i in 1:nrow(A)) {
-	  # calculate y values for current equation
+	for (i in 1:neq) {
+# 	  plot(xlim, ylim, type="n", xlab = vars[1], ylab = vars[2], xlim = xlim, ylim = ylim)
+#
+# 	  if (A[i,2] == 0) abline( v = b[i] / A[i,1], col = col[i], lwd = lwd[i], lty = lty[i] )
+# 	  else {
+# 	    # calculate y values for current equation
+# 	    y <- (b[i] - A[i,1] * x) / A[i,2]
+# 	    lines( x, y, col = col[i], type = 'l', lwd = lwd[i], lty = lty[i] )
+# 	  }
+
+	  	  # calculate y values for current equation
 	  # if A[i,2]==0 this will give Inf, so have to use abline()
 	  y <- (b[i] - A[i,1] * x) / A[i,2]
 	  if (i == 1)
@@ -75,10 +91,10 @@ plotEqn <- function(A, b, vars, xlim=c(-4, 4), ylim,
 	      xlab = vars[1], ylab = vars[2], xlim = xlim, ylim = ylim
 	    )
 	  else
-	  if (A[i,2] == 0)
+	    if (A[i,2] == 0)
 	      # a vertical line
 	      abline( v = b[i] / A[i,1], col = col[i], lwd = lwd[i], lty = lty[i] )
-	  else
+	    else
 	    lines( x, y, col = col[i], type = 'l', lwd = lwd[i], lty = lty[i] )
 
 	  if (!is.null(labels)) {
@@ -88,6 +104,15 @@ plotEqn <- function(A, b, vars, xlim=c(-4, 4), ylim,
 	  }
 	}
 	if (axes) abline(h=0, v=0, col="gray")
+
+	if (solution) {
+	  for (i in 1:neq-1) {
+	    for (j in i:neq) {
+	      x <- try(solve(A[c(i,j),],b[c(i,j)]), silent=TRUE)
+	      if (!inherits(x, "try-error")) points(x[1], x[2], cex=1.5)
+	    }
+	  }
+	}
 }
 
 
