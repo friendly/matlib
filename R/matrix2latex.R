a8b932a3c10a34711318c74c506f160e7193357f
Phil Chalmers <rphilip.chalmers@gmail.com>
2016-03-14 21:50:34 -0400

    matrix2latex function

---
+++ R/matrix2latex.R
@@ -0,0 +1,31 @@
+#' Convert matrix to LaTeX equation
+#'
+#' This function provides a soft-wrapper to \code{xtable::xtableMatharray()} with support for
+#' \code{fraction}s output.
+#'
+#' @param X a matrix
+#' @param fractions logical; if \code{TRUE}, try to express non-integers as rational numbers
+#' @param ... additional arguments passed to \code{xtable::xtableMatharray()}
+#' @return returns an object of class 'xtableMatharray'
+#' @author Phil Chalmers
+#' @examples
+#' A <- matrix(c(2, 1, -1,
+#'              -3, -1, 2,
+#'              -2,  1, 2), 3, 3, byrow=TRUE)
+#' b <- c(8, -11, -3)
+#'
+#' matrix2latex(cbind(A,b))
+#' matrix2latex(cbind(A,b), digits = 0)
+#' matrix2latex(cbind(A/2,b), fractions = TRUE)
+#'
+matrix2latex <- function(x, fractions = FALSE, ...){
+  if (fractions) {
+    mass <- requireNamespace("MASS", quietly=TRUE)
+    if (!mass) stop("fractions=TRUE needs MASS package")
+  }
+  xtable <- requireNamespace("xtable", quietly=TRUE)
+  if (!xtable) stop("latex=TRUE needs xtable package")
+  ret <- if (fractions) xtable::xtableMatharray(as.character(MASS::fractions(x)), ...)
+    else xtable::xtableMatharray(x, ...)
+  ret
+}
