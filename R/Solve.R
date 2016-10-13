2034233d6e0b687864ae5ae5050a1a46f73510ab
Michael Friendly <friendly@yorku.ca>
2016-10-13 10:02:50 -0400

    add default for b to Solve; ammend documentation; closes # 14

--- R/Solve.R
+++ R/Solve.R
@@ -8,8 +8,14 @@
 #' \eqn{A} and right-hand side vector \eqn{b}, using \code{link{gaussianElimination}}.
 #' Display the solutions using \code{\link{showEqn}}.
 #'
+#' @details This function mimics the base function \code{\link[base]{solve}} when supplied with two arguments,
+#'    \code{(A, b)}, but gives a prettier result, as a set of equations for the solution.  The call
+#'    \code{solve(A)} with a single argument overloads this, returning the inverse of the matrix \code{A}.
+#'    For that sense, use the function \code{\link{inv}} instead.
+#'
 #' @param A, the matrix of coefficients of a system of linear equations
-#' @param b, the vector of constants on the right hand side of the equations
+#' @param b, the vector of constants on the right hand side of the equations. The default is a vector of zeros,
+#'        giving the homogeneous equations \eqn{Ax = 0}.
 #' @param vars a numeric or character vector of names of the variables.
 #'        If supplied, the length must be equal to the number of unknowns in the equations.
 #'        The default is \code{paste0("x", 1:ncol(A)}.
@@ -20,7 +26,7 @@
 #' @return the function is used primarily for its side effect of printing the solution in a
 #'  readable form, but it invisibly returns the solution as a character vector
 #' @author John Fox
-#' @seealso \code{\link{gaussianElimination}}, \code{\link{showEqn}}
+#' @seealso \code{\link{gaussianElimination}}, \code{\link{showEqn}} \code{\link{inv}}, \code{\link[base]{solve}}
 #' @examples
 #'   A1 <- matrix(c(2, 1, -1,
 #'                -3, -1, 2,
@@ -35,8 +41,8 @@
 #'   b3 <- c(1, 2, 4)
 #'   Solve(A2, b3, fractions=TRUE) # overdetermined
 
-Solve <- function(A, b, vars, verbose=FALSE, simplify=TRUE, fractions=FALSE, ...){
+Solve <- function(A, b = rep(0, nrow(A)), vars, verbose=FALSE, simplify=TRUE, fractions=FALSE, ...){
   solution <- gaussianElimination(A, b, verbose=verbose, fractions=fractions, ...)
-  showEqn(solution[, - ncol(solution)], solution[, ncol(solution)], vars, simplify=simplify,
+  showEqn(solution[, - ncol(solution)], solution[, ncol(solution)], vars=vars, simplify=simplify,
           fractions=fractions, ...)
 }
