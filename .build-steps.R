79d908451da7f2407b93c1435ac48fc9dfaac1fa
Michael Friendly <friendly@yorku.ca>
2021-08-18 12:44:10 -0400

    .rbuildignore conflicts

--- .build-steps.R
+++ .build-steps.R
@@ -2,15 +2,17 @@
 Sys.setenv(RGL_USE_NULL = TRUE)
 
 # The standard R Studio / devtools build does not compress vignettes. To avoid the warning
-# 
+#
 # 'gs+qpdf' made some significant size reductions
-# 
+#
 # it was necessary to build manually, using
 
 #Sys.setenv(R_GSCMD="C:/Program Files/gs/gs9.21/bin/gswin64c.exe")
 Sys.setenv(R_GSCMD="C:/Program Files/gs/gs9.53.3/bin/gswin64c.exe")
 
-devtools::build(args = c('--resave-data','--compact-vignettes=both'))
+args = c('--resave-data','--compact-vignettes=both')
+devtools::check()
+devtools::build(args = args)
 
 #devtools::build(args = c('--compact-vignettes=both'))
 
