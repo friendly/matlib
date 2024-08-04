797ed82fe0aa461f4175c6cffb079c4e00322fa8
philchalmers <rphilip.chalmers@gmail.com>
2024-08-04 15:43:08 -0400

    add example and document

--- dev/Eqn.R
+++ dev/Eqn.R
@@ -1,16 +1,24 @@
-#' Create a LaTeX equation
+#' Create a LaTeX equation wrapper
 #'
-#' @param ...
-#' @param number
+#' @param ... expressions using \code{\link{cat}} to be wrapped in a LaTeX math
+#'   environment
+#' @param number logical; include equation number?
 #' @param label character vector specifying the LaTeX label to use (e.g., \code{eqn:myeqn})
+#' @export
 #' @examples
-#' Eqn(cat('y=mc^2'))
-#' Eqn(cat('y=mc^2'), number = FALSE)
-#' Eqn(cat('y=mc^2'), label = 'eqn:einstein')
+#' Eqn(cat('e=mc^2'))
+#' Eqn(cat('e=mc^2'), number = FALSE)
+#' Eqn(cat('e=mc^2'), label = 'eqn:einstein')
 #'
 #' Eqn(cat("X=U \\lambda V"), label='eqn:svd')
-
-
+#'
+#' # expressions that use cat() within their calls
+#' Eqn({
+#'     symbolicMatrix("u", "n", "k", lhs = 'SVD=')
+#'     symbolicMatrix("\\lambda", "k", "k", diag=TRUE)
+#'     symbolicMatrix("v", "k", "p", transpose = TRUE)
+#' })
+#'
 Eqn <- function(..., number = TRUE, label = NULL) {
   wrap <- if(number) "equation" else "equation*"
   cat(sprintf("\n\\begin{%s}\n", wrap))
