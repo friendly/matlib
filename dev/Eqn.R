f983804b825c8de828526a848c26be5c7e8da7ca
philchalmers <rphilip.chalmers@gmail.com>
2024-08-05 22:14:44 -0400

    parse all regardless of arg order

--- dev/Eqn.R
+++ dev/Eqn.R
@@ -1,12 +1,9 @@
 #' Create a LaTeX equation wrapper
 #'
-#' @param expr expressions using \code{\link{cat}} to be wrapped in a LaTeX math
+#' @param ... expressions using \code{\link{cat}} and
+#'   return a \code{NULL} object to be wrapped in a LaTeX math
 #'   environment. Supplying a character vector will automatically wrap the
 #'   expression inside a call to \code{\link{cat}}
-#' @param ... other functions called print their output using \code{\link{cat}}
-#'   and return a \code{NULL} object. Note that if character vectors are supplied
-#'   in this argument they must be explicitly wrapped within \code{\link{cat}} to
-#'   print correctly
 #' @param number logical; include equation number?
 #' @param label character vector specifying the LaTeX label to use (e.g., \code{eqn:myeqn})
 #' @param align logical; use the \code{align} environment with explicit \code{&}
@@ -44,16 +41,21 @@
 #'
 #' matrix2latex(cbind(A,b)) |> Eqn()
 #'
-Eqn <- function(expr, ..., number = TRUE, label = NULL, align = FALSE) {
+Eqn <- function(..., number = TRUE, label = NULL, align = FALSE) {
   wrap <- if(align) "align" else "equation"
   if(!number) wrap <- paste0(wrap, '*')
   cat(sprintf("\n\\begin{%s}\n", wrap))
   if(!is.null(label))
     cat(sprintf('\\label{%s}\n', label))
-  if(is.character(expr)) cat(expr)
-  dots <- list(...)
-  if(length(dots)) eval(expr)
+  tmp <- substitute(deparse(...))
+  is_char <- sapply(tmp, is.character)[-1L]
+  chartmp <- as.character(tmp)[-1L]
+  for(i in 1L:length(chartmp)){
+      if(is_char[i]) cat(chartmp[i])
+      else eval(parse(text = chartmp[i]))
+  }
   cat(sprintf("\\end{%s}\n", wrap))
+  invisible(NULL)
 }
 
 Eqn_newline <- function(form = ' \\\\ \n') cat(form)
