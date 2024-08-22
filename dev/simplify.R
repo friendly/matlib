5df0d2765b52ecdcb5cfcb5723d1f1a143f9497e
John Fox <jfox@mcmaster.ca>
2024-08-22 11:20:31 -0400

    add simplify.latexMatrix() method to dev/simplify.R

--- dev/simplify.R
+++ dev/simplify.R
@@ -1,35 +1,48 @@
-simplify <- function(string, simplify0s=TRUE, simplify1s=TRUE, 
+simplify <- function(object, simplify0s=TRUE, simplify1s=TRUE, 
                      sparse=FALSE, ...){
   UseMethod("simplify")
 }
 
-simplify.default <- function(string, simplify0s=TRUE, simplify1s=TRUE, 
+simplify.default <- function(object, simplify0s=TRUE, simplify1s=TRUE, 
                              sparse=FALSE, ...){
   if (simplify1s){
     # remove leading 1
-    string <- gsub(" *[^0-9]1 *\\\\cdot *", " ", 
-                   paste0(" ", string) )
+    object <- gsub(" *[^0-9]1 *\\\\cdot *", " ", 
+                   paste0(" ", object) )
     # removing trailing 1
-    string <- gsub(" *\\\\cdot *1 {1,}", " ", 
-                   paste0(string, " ")) 
+    object <- gsub(" *\\\\cdot *1 {1,}", " ", 
+                   paste0(object, " ")) 
   }
   if (simplify0s){
     # remove items with leading 0
-    string <- gsub(" *[^1-9_{}}]*0 \\\\cdot (\\(*.*\\))* *[^+-]*", 
-                   " ", string)
+    object <- gsub(" *[^1-9_{}}]*0 \\\\cdot (\\(*.*\\))* *[^+-]*", 
+                   " ", object)
     # remove items with trailing 0
-    string <- gsub(" *(\\(*.*\\))*.*\\\\cdot *[^1-9_{}}]*0", 
-                   " ", string) 
-    if (sparse){
-      string <- trimws(string)
-        string <- sub("^0$", "", string)
-      } else {
-      string <- sub("^ *$", "0", string) # replace empty with 0
+    object <- gsub(" *(\\(*.*\\))*.*\\\\cdot *[^1-9_{}}]*0", 
+                   " ", object) 
+    object <- if (sparse){
+      object <- trimws(object)
+      sub("^0$", "", object) # replace 0 with empty
+    } else {
+      sub("^ *$", "0", object) # replace empty with 0
     }
   }
-  string <- sub("^ *\\+ *", "", string) # remove stray + 
-  string <- trimws(string)
-  string
+  object <- sub("^ *\\+ *", "", object) # remove stray + 
+  object <- trimws(object)
+  object
+}
+
+simplify.latexMatrix <- function(object, simplify0s=TRUE, simplify1s=TRUE, 
+                             sparse=FALSE, ...){
+  X <- getBody(object)
+  dim <- dim(X)
+  X <- sapply(X, simplify, simplify0s=simplify0s, 
+              simplify1s=simplify1s, sparse=sparse)
+  X <- matrix(X, dim[1], dim[2])
+  X <- latexMatrix(X)
+  X <- matlib:::updateWrapper(X, getWrapper(object))
+  X$dim <- Dim(object)
+  X
 }
 
 if (FALSE){
@@ -47,4 +60,13 @@ simplify("(a + b) \\cdot 1")
 simplify("(a + b) \\cdot 0")
 simplify("0 \\cdot (a + b)", sparse=TRUE)
 simplify("(a + b) \\cdot 1", sparse=TRUE)
+
+C <- latexMatrix(matrix(c(0, 1, 0, 0,
+                          0, 0, 1, 0), nrow=2, byrow=TRUE), 
+                 matrix = "bmatrix")
+B <- latexMatrix('\\beta', ncol = 3, nrow=4, 
+                 comma=TRUE, prefix.col = 'y_',
+                 zero.based=c(TRUE, FALSE))
+C %*% B
+simplify(C %*% B)
 }
