3dbdf71e2fa607bf3d1b3d27bbac58fc3dfc9643
philchalmers <rphilip.chalmers@gmail.com>
2024-08-24 22:23:45 -0400

    add sprintEqn; requires further testing and features

--- dev/eqn_parser.R
+++ dev/eqn_parser.R
@@ -1,5 +1,5 @@
 eqn_parser <- function(string,
-                       `**`="boldsymbol", `*`="mathcal", `%`="mathrm"){
+                       `**`="boldsymbol", `*`="mathcal", ...){
 
     # `*` -> \times
     if(grepl('`*`', string))
@@ -7,22 +7,14 @@ eqn_parser <- function(string,
 
     # %n -> \\\\ \n
     if(grepl('%n', string))
-        string <- gsub('%n', "\\\\ \n", string, fixed=TRUE)
-
-    # %.%
-    if(grepl('\\%', string)){
-        loc <- gregexpr("\\%", string)[[1L]]
-        for(i in loc[seq(length(loc), 2, by=-2)])
-            substring(string, i, i+1L) <- '}'
-        string <- gsub('\\%', replacement = sprintf("\\\\%s{", `%`), string)
-    }
+        string <- gsub('%n', "\\\\ ", string, fixed=TRUE)
 
     # **.**
     if(grepl('\\*\\*', string)){
         loc <- gregexpr("\\*\\*", string)[[1L]]
         for(i in loc[seq(length(loc), 2, by=-2)])
-            substring(string, i, i+1L) <- '}%'
-        string <- gsub('%', '', string)
+            substring(string, i, i+1L) <- '} '
+        string <- gsub('} ', '}', string, fixed = TRUE)
         string <- gsub('\\*\\*', replacement = sprintf("\\\\%s{", `**`), string)
     }
 
@@ -45,8 +37,7 @@ if(FALSE){
     s4 <- "*H*_0 : *C B* & = "
     s5 <- "*H*_0 : *C* *B* & = "
     s6 <- "*H*_0 : %n *C* *B* & = "
-    s7 <- "%H%_0 : %C% %B% = "
-    s8 <- "*H*_0 : *C* `*` *B*  = "
+    s7 <- "*H*_0 : *C* `*` *B*  = "
 
     identical(eqn_parser(s1), s1)
     eqn_parser(s2)
@@ -56,6 +47,71 @@ if(FALSE){
     eqn_parser(s5)
     eqn_parser(s6)
     eqn_parser(s7)
-    eqn_parser(s8)
+}
+
+
+
+
+sprintEqn <- function(string, mats, ...){
+    string <- paste0(string, ' ')
+    string <- eqn_parser(string)
+    bodies <- lapply(mats, getLatex)
+    bodies <- lapply(bodies, \(x) gsub("\\", "\\\\", x, fixed=TRUE))
+    nms <- names(bodies)
+    for(i in seq_len(length(nms)))
+        string <- gsub(paste0('%', nms[i], " "), bodies[[i]], string)
+    Eqn(string, ...)
+}
+
+if(FALSE){
+
+    # from vignette
+    Eqn("\\mathbf{X} & = \\mathbf{U} \\mathbf{\\Lambda} \\mathbf{V}^\\top",
+        Eqn_newline(),
+        ' & =',
+        latexMatrix("u", "n", "k"),
+        latexMatrix("\\lambda", "k", "k", diag=TRUE),
+        latexMatrix("v", "k", "p", transpose = TRUE),
+        align=TRUE)
+
+    sprintEqn("**X** & = **U \\Lambda V**^\\top %n
+                     & =  %U    %L    %V",
+              list(U=latexMatrix("u", "n", "ks"),
+                   V=latexMatrix("v", "k", "p", transpose = TRUE),
+                   L=latexMatrix("\\lambda", "k", "k", diag=TRUE)),
+              align=TRUE, label='eq:svd')
+
+    # next
+    A <- latexMatrix(matrix(c(1, -3, 0, 1), 2, 2))
+    B <- latexMatrix(matrix(c(5, 3, -1, 4), 2, 2))
+    C <- latexMatrix(symbol="c", 2, 3)
+    D <- latexMatrix(symbol="d", 2, 3)
+
+    Eqn("\\mathbf{A} + \\mathbf{B} =", A, " + ", B, " = ", A + B, " = ", as.double(A + B))
+    sprintEqn("**A** + **B** = %A + %B = %C = %D",
+              list(A=A, B=B, C=A + B, D=latexMatrix(as.double(A+B))))
+
+    # last
+    (C <- latexMatrix(matrix(c(0, 1, 0, 0,
+                               0, 0, 1, 0), nrow=2, byrow=TRUE),
+                      matrix = "bmatrix"))
+    B0 <- latexMatrix('\\beta', ncol = 3, nrow=2, comma=TRUE, prefix.col = 'y_')
+    Eqn("\\mathcal{H}_0 : \\mathbf{C} \\mathbf{B} & = ",
+        C, B,
+        Eqn_newline(),
+        '& =',
+        B0,
+        "= \\mathbf{0}_{(2 \\times 3)}",
+        align=TRUE)
+
+    sprintEqn("*H*_0 : **C B** & = %C %B \\\\
+                               & = %D = **0**_{(2 \\times 3)} ",
+              list(C=C, B=B, D=B0))
+
+    # TODO, allow matching of % elements with more than one character, such as
+    # %A + %B = %AB
+    # vs current
+    # %A + %B = %C
+
 
 }
