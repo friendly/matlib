c22a9e4e750564487402eff62f0b94a12a8aa74e
John Fox <jfox@mcmaster.ca>
2024-09-08 12:57:44 -0400

    move bordered matrix code to R/ and document

--- R/latexMatrix.R
+++ R/latexMatrix.R
@@ -91,6 +91,15 @@
 #'               that many rows.
 #' @param ncol   Number of columns, a single character representing columns symbolically, or an integer, generating
 #'               that many columns.
+#' @param rownames optional vector of names for the matrix rows.
+#'        if \code{symbol} is an R matrix with row names, these are used.
+#'        For a matrix with a non-numeric (e.g., \code{"m"}) number of rows,
+#'        3 names should be supplied, for the 1st, 2nd, and last rows.
+#' @param colnames optional vector of names for the matrix columns.
+#'        if \code{symbol} is an R matrix with column names, these are used.
+#'        For a matrix with a non-numeric (e.g., \code{"n"}) number of columns,
+#'        3 names should be supplied, for the 1st, 2nd, and last columns.
+
 #' @param matrix Character string giving the LaTeX matrix environment used in \code{\\begin{}}, \code{\\end{}}. Typically one of:
 #' \describe{
 #'    \item{\code{"pmatrix"}}{uses parentheses: \code{"(", ")"}}
@@ -236,12 +245,22 @@
 #' X[1:2, ]
 #' X[-(1:2), ]
 #' X[1:2, 2]
+#' 
+#' # defining row and column names
+#' W <- latexMatrix(rownames=c("\\alpha_1", "\\alpha_2", "\\alpha_m"),
+#'                  colnames=c("\\beta_1", "\\beta_2", "\\beta_n"))
+#' W
+#' Rownames(W) <- c("\\mathrm{Abe}", "\\mathrm{Barry}", "\\mathrm{Zelda}")
+#' Colnames(W) <- c("\\mathrm{Age}", "\\mathrm{BMI}", "\\mathrm{Waist}")
+#' W
 
 
 latexMatrix <- function(
     symbol="x",
     nrow="n",
     ncol="m",
+    rownames=NULL,
+    colnames=NULL,
     matrix=getOption("latexMatrixEnv"),
     diag=FALSE,
     sparse=FALSE,
@@ -257,8 +276,8 @@ latexMatrix <- function(
     suffix="",
     prefix.row="",
     prefix.col=""
-    ){
-
+){
+  
   latexFraction <- function(x){
     negative <- grepl("-", x)
     if (negative) x <- sub("-", "", x)
@@ -271,25 +290,31 @@ latexMatrix <- function(
   }
   
   if (is.null(matrix)) matrix <- "pmatrix"
-
+  
   end.at.n.minus.1 <- gsub(" ", "", end.at) == c("n-1", "m-1")
-
+  
   if (isTRUE(transpose)) transpose <- "\\top"
   if (!missing(exponent) && !isFALSE(transpose)){
     exponent <- paste0("{", exponent, "^", transpose, "}")
     transpose <- FALSE
   }
-
+  
   # start composing output string:
-
+  
   result <- paste0(if (fractions) "\\renewcommand*{\\arraystretch}{1.5} \n",
                    # if (!missing(lhs)) paste0(lhs, " = \n"),
                    "\\begin{", matrix, "} \n"
   )
-
+  
   # matrix input:
-
+  
   if (is.matrix(symbol)){
+    dimnames <- dimnames(symbol)
+    if (!is.null(dimnames) && is.null(rownames) 
+        && is.null(colnames)){
+      rownames <- dimnames[[1]]
+      colnames <- dimnames[[2]]
+    }
     if (is.numeric(symbol)){
       if (is.null(digits) && all(trunc(symbol) == symbol) ) digits <- 0
       if (fractions) {
@@ -316,44 +341,44 @@ latexMatrix <- function(
         result <- paste0(result, mat[i, j], if (j == nc) " \\\\ \n" else " & " )
       }
     }
-
+    
   } else {
-
+    
     # character symbol supplied to construct matrix elements:
-
+    
     if (!(is.character(symbol) && is.vector(symbol)
           && length(symbol) == 1))
       stop("'symbol' must be a single character string or a matrix. Hint: wrap a vector in matrix(), with nrow=1 or ncol=1.")
-
+    
     if (is.numeric(nrow)){
       if (round(nrow) != nrow || nrow <= 0)
         stop("nrow is not a positive whole number")
     }
-
+    
     if (is.numeric(ncol)){
       if (round(ncol) != ncol || ncol <= 0)
         stop("ncol is not a positive whole number")
     }
-
+    
     comma <- if (comma) "," else ""
-
+    
     row.elements <- c(symbol, symbol, "\\cdots", symbol) # row without subscripts
-
+    
     col.subscripts <- if (!zero.based[2]) { # subscripts for a column
       c("1", "2", "", ncol)
     } else {
       c("0", "1", "", if (is.numeric(ncol)) ncol - 1
         else paste0(ncol, if (end.at.n.minus.1[2]) " - 1" else ""))
     }
-
+    
     left.sub <- c("_{", "_{", "", "_{") # start of subscript
     right.sub <- c("}", "}", "", "}") # end of subscript
     post.element <- c(" & ", " & ", " & ", " \\\\ \n") # cell separator, end of row
-
+    
     if (diag){
-
+      
       # diagonal matrix:
-
+      
       zero <- paste0("0", paste(rep(" ",
                                     nchar(symbol) + 3 + nchar(prefix) + nchar(suffix)),
                                 collapse=""))
@@ -396,11 +421,11 @@ latexMatrix <- function(
                            if (j == nrow) " \\\\ \n" else " & ")
         }
       }
-
+      
     } else if (is.character(nrow)){
-
+      
       # non-numeric number of rows:
-
+      
       vdots <- paste0("\\vdots",
                       paste0(paste(rep(" ",
                                        nchar(symbol) + nchar(prefix) + nchar(suffix) - 1),
@@ -411,11 +436,11 @@ latexMatrix <- function(
       } else {
         c("0", "1", "", paste0(nrow, if (end.at.n.minus.1[1]) " - 1" else ""))
       }
-
+      
       if (is.character(ncol)){
-
+        
         # non-numeric number of rows, non-numeric number of columns:
-
+        
         vdots <- paste0(vdots, " & ", vdots, " & ",
                         if (nrow != ncol) "       & " else "\\ddots & ",
                         vdots, " \\\\ \n")
@@ -433,9 +458,9 @@ latexMatrix <- function(
           }
         }
       } else {
-
+        
         # non-numeric number of rows, numeric number of columns:
-
+        
         vdots <- paste0(paste(rep(vdots, ncol), collapse = " & "), " \\\\ \n")
         for (i in 1:4){
           result <- paste0(result, "  ")
@@ -455,11 +480,11 @@ latexMatrix <- function(
           }
         }
       }
-
+      
     } else if (is.character(ncol)){
-
+      
       # numeric number of rows, non-numeric number of columns:
-
+      
       for (i in 1:nrow){
         result <- paste0(result, "  ")
         for (j in 1:4){
@@ -471,34 +496,34 @@ latexMatrix <- function(
                            post.element[j])
         }
       }
-
+      
     } else {
-
+      
       # numeric number of rows, numeric number of columns:
-
+      
       is_scalar <- nrow == 1 && ncol == 1
-
+      
       for (i in 1:nrow){
         result <- paste0(result, "  ")
         for (j in 1:ncol){
           result <- paste0(result, prefix, symbol,
                            if(!is_scalar) "_{",
                            if (nrow > 1)
-                               if(prefix.row != '') paste0(prefix.row, '{', i - zero.based[1], '}')
-                               else i - zero.based[1],
+                             if(prefix.row != '') paste0(prefix.row, '{', i - zero.based[1], '}')
+                           else i - zero.based[1],
                            if (nrow > 1 && ncol > 1) comma,
                            if (ncol > 1)
-                               if(prefix.col != '') paste0(prefix.col, '{', j - zero.based[2], '}')
-                               else j - zero.based[2],
+                             if(prefix.col != '') paste0(prefix.col, '{', j - zero.based[2], '}')
+                           else j - zero.based[2],
                            if(!is_scalar) "}", suffix,
                            if (j == ncol) " \\\\ \n" else " & ")
         }
       }
     }
   }
-
+  
   # complete output string, adding optional decorations:
-
+  
   mat.result <- paste0(result, "\\end{", matrix, "}",
                        if (show.size) paste0("_{(",
                                              nrow,
@@ -510,26 +535,50 @@ latexMatrix <- function(
                        if (!missing(exponent)) paste0("^{", exponent, "}"),
                        if (!isFALSE(transpose)) paste0("^", transpose),
                        "\n")
-
+  
   x.mat <- strsplit(mat.result, "\\n")[[1]]
   pick <- c(1, length(x.mat))
   wrapper <- x.mat[pick] # LaTeX matrix environment
   body <- x.mat[-pick]
   body <- gsub('\\\\\\\\', '', body)
-  # body <- gsub(' ', '', body)
   splt <- sapply(body, function(x.mat) strsplit(x.mat, '&'))
   nrow.x <- length(splt)
   body <- unname(do.call(rbind, splt)) # matrix of LaTeX cells
   body <- sub(" *$", "", sub("^ *", "", body))
   if(sparse)
-      mat.result <- gsub('[[:blank:]]+0[[:blank:]]+', ' ', mat.result)
-
+    mat.result <- gsub('[[:blank:]]+0[[:blank:]]+', ' ', mat.result)
+  
+  if (!is.null(rownames)){
+    rownames <- as.character(rownames)
+    if (!is.numeric(nrow)){
+      if (length(rownames) != 3) 
+        stop("there should be 3 row names")
+      rownames <- c(rownames[1:2], "\\vdots", rownames[3])
+    } else {
+      if (length(rownames) != nrow) 
+        stop("there should be ", nrow, " row names")
+    }
+  }
+  
+  if (!is.null(colnames)){
+    colnames <- as.character(colnames)
+    if (!is.numeric(ncol)){
+      if (length(colnames) != 3) 
+        stop("there should be 3 column names")
+      colnames <- c(colnames[1:2], "\\cdots", colnames[3])
+    } else {
+      if (length(colnames) != ncol) 
+        stop("there should be ", ncol, " column names")
+    }
+  }
+  
   # "latexMatrix" object:
-
+  
   result <- list(matrix = mat.result,
                  dim = c(nrow, ncol),
                  body = body,
-                 wrapper = wrapper)
+                 wrapper = wrapper,
+                 dimnames = list(rownames=rownames, colnames=colnames))
   class(result) <- 'latexMatrix'
   result
 }
@@ -670,6 +719,76 @@ Ncol.latexMatrix <- function(x, ...){
 #' @rdname latexMatrix
 #' @export
 print.latexMatrix <- function(x, onConsole=TRUE,  ...){
+  
+  countChars <- function(string, adjust=TRUE){
+    gsub("\\\\[[:alpha:]]*", if(adjust) "X" else "", string) |> 
+      gsub("[_^{}]*", "", x = _) |>
+      nchar()
+  }
+  
+  if (!is.null(rownames(x)) || !is.null(colnames(x))){
+    rownames <- rownames(x)
+    colnames <- colnames(x)
+    X <- getBody(x)
+    
+    # adjust size of column names
+    max.col <- apply(countChars(X), 2, max)
+    if (!is.null(colnames)){
+      for (j in 1:length(colnames)){
+        nchar <- if ("\\cdots" == colnames[j]) {
+          3
+        } else {
+          countChars(colnames[j], adjust=FALSE) 
+        }
+        pad <- max(max.col[j] - nchar, 0) 
+        colnames[j] <- paste0(colnames[j], 
+                              paste(rep("~", pad), collapse=""))
+      }
+    }
+    if (!is.null(colnames)){
+      nchar.colnames <- countChars(colnames, adjust=FALSE)
+      for (i in 1:nrow(X)){
+        for (j in 1:ncol(X)){
+          xij <- X[i, j]
+          nchar <- if ("\\cdots" == xij) {
+            3
+          } else if ("\\vdots" == xij) {
+            NA
+          } else if ("\\ddots" == xij) {
+            NA
+          } else {
+            countChars(xij)
+          }
+          pad <- max(nchar.colnames[j] - nchar, 0)
+          if (!is.na(pad) && pad > 0) {
+            X[i, j] <- paste0("\\phantom{",
+                              paste(rep("e", pad), collapse=""), 
+                              "}", xij)
+          }
+        }
+      }
+      XX <- latexMatrix(X, colnames=colnames, rownames=rownames)
+      XX <- updateWrapper(XX, getWrapper(x))
+      XX$dim <- Dim(x)
+      x <- XX
+    }
+    
+    latex <- getLatex(x)
+    latex <- paste0("\\begin{matrix}\n",
+                    if (!is.null(rownames))  "  & ",
+                    if (!is.null(colnames)) paste0(" \\begin{matrix} ", 
+                                                   paste(colnames, collapse=" & "), 
+                                                   "\n  \\end{matrix} \\\\ \n"),
+                    if (!is.null(rownames)) paste0(" \\begin{matrix}  \n", 
+                                                   paste(paste0("   ", 
+                                                                rownames, "\\\\ \n"), 
+                                                         collapse=""),
+                                                   "\\end{matrix}  & \n"),
+                    latex, "\\\\ \n",
+                    "\\end{matrix} \n"
+    )
+    x$matrix <- latex
+  }
   if (onConsole) cat(getLatex(x))
   invisible(x)
 }
@@ -762,6 +881,93 @@ rbind.latexMatrix <- function(..., deparse.level){
     X <- latexMatrix(X)
     return(updateWrapper(X, getWrapper(matrices[[1]])))
   }
+}
+
+#' @rdname latexMatrix
+#' @export
+dimnames.latexMatrix <- function(x){
+  x$dimnames
+}
+
+#' @param value for \code{"Dimnames<-()"}, a two-element list with,
+#'        respectively, character vectors of row and column names;
+#'        for  \code{"Rownames<-()"} and \code{"Colnames<-()"}, a
+#'        vector of names.
+#' @rdname latexMatrix
+#' @export
+`Dimnames<-` <- function (x, value) {
+  UseMethod("Dimnames<-")
+}
+
+#' @rdname latexMatrix
+#' @export
+`Dimnames<-.latexMatrix` <- function (x, value) {
+  oldnames <- dimnames(x)
+  if (!is.list(value) || length(value) != 2)
+    stop("'value' must be a 2-element list with row and column names")
+  dim <- Dim(x)
+  nrow <- dim[1]
+  ncol <- dim[2]
+  rownames <- value[[1]]
+  colnames <- value[[2]]
+  
+  if (!is.null(rownames) && (length(rownames) > 1 || rownames != "")){
+    if (!is.numeric(nrow)){
+      if (length(rownames) != 3) 
+        stop("there should be 3 row names")
+      rownames <- c(rownames[1:2], "\\vdots", rownames[3])
+    } else {
+      if (length(rownames) != nrow) 
+        stop("there should be ", nrow, " row names")
+    }
+  }
+  
+  if (!is.null(colnames) && (length(colnames) > 1 || colnames != "")){
+    if (!is.numeric(ncol)){
+      if (length(colnames) != 3) 
+        stop("there should be 3 column names")
+      colnames <- c(colnames[1:2], "\\cdots", colnames[3])
+    } else {
+      if (length(colnames) != ncol) 
+        stop("there should be ", ncol, " column names")
+    }
+  }
+  
+  newnames <- oldnames
+  if (!is.null(rownames) && (length(rownames) > 1 || rownames != "")) newnames$rownames <- rownames
+  if (is.null(rownames)) newnames["rownames"] <- list(NULL)
+  if (!is.null(colnames) && (length(colnames) > 1 || colnames != "")) newnames$colnames <- colnames
+  if (is.null(colnames)) newnames["colnames"] <- list(NULL)
+  x$dimnames <- newnames
+  x
+}
+
+#' @rdname latexMatrix
+#' @export
+`Rownames<-` <- function(x, value){
+  UseMethod("Rownames<-")
+}
+
+#' @rdname latexMatrix
+#' @export
+`Rownames<-.latexMatrix` <- function(x, value){
+  names <- list(rownames=value, colnames="")
+  Dimnames(x) <- names
+  x
+}
+
+#' @rdname latexMatrix
+#' @export
+`Colnames<-` <- function(x, value){
+  UseMethod("Colnames<-")
+}
+
+#' @rdname latexMatrix
+#' @export
+`Colnames<-.latexMatrix` <- function(x, value){
+  names <- list(rownames="", colnames=value)
+  Dimnames(x) <- names
+  x
 }
 
 # unexported functions:
