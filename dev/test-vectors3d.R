52699f130393f75aa251001d3076552b689a5408
Michael Friendly <friendly@yorku.ca>
2024-10-17 18:42:40 -0400

    test dev/vectors3d

---
+++ dev/test-vectors3d.R
@@ -0,0 +1,32 @@
+---
+  title: "Test plot math in vectors3d"
+---
+  
+library(rgl)
+library(matlib)
+
+# want to use labels that are expressions:
+labs <- c(expression(x[1]), "y", expression(x[2]))
+is.expression(labs)
+
+source(here::here("dev", "vectors3d.R"))
+
+open3d()
+E <- diag(3)
+rownames(E) <- c(expression(x[1]), "y", expression(x[2]))
+vectors3d(E, lwd=2)
+vectors3d(c(1, 1, 1),
+          labels=c("",expression(hat(y))), color="red",
+          lwd=3)
+vectors3d(c(1, 1, 0),
+          labels=c("", "x+y"),
+          color="green", lwd=2)
+planes3d(0, 0, 1, 0, col="gray",
+         alpha=0.1)
+segments3d(rbind(c(1, 1, 1),
+                 c(1, 1, 0)))
+arc(c(1, 1, 1), c(0, 0, 0),
+    c(1, 1, 0))
+corner(c(0, 0, 0), c(1, 1, 0), c(1, 1, 1))
+
+
