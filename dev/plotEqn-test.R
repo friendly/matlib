977c930066eb5b0eeeab035af34bcc1560d6e7ac
Michael Friendly <friendly@yorku.ca>
2024-10-21 17:38:28 -0400

    intersections function in plotEqn-test

--- dev/plotEqn-test.R
+++ dev/plotEqn-test.R
@@ -20,3 +20,22 @@ plotEqn(A, b, vars = c("x", "y"),
         labels = c("y = x - 2",
                    "y = 1/2 - x",
                    "y = 1"))
+
+# bring out code to find intersections to a function
+
+intersections <- function(A, b) {
+  neq <- nrow(A)
+  res <- matrix(NA, nrow=neq*(neq - 1)/2, ncol=2)
+  colnames(res) <- c("x", "y")
+  k <- 0
+  for (i in 1:(neq - 1)) {
+    for (j in (i + 1):neq) {
+      k <- k + 1
+      x <- try(solve(A[c(i, j), ], b[c(i, j)]), silent=TRUE)
+      if (!inherits(x, "try-error")) res[k, ] <- x
+    }
+  }
+  res
+}
+
+intersections(A,b)
