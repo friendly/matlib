78c7ea2e7953076260bb0df7b751d45aa638cf29
Michael Friendly <friendly@yorku.ca>
2024-11-01 13:12:39 -0400

    test-vectors3d

--- dev/test-vectors3d.R
+++ dev/test-vectors3d.R
@@ -5,12 +5,29 @@
 library(rgl)
 library(matlib)
 
+
+# problem with `col =` in current version when more than one vector is given
+
+open3d()
+E <- diag(3)
+rownames(E) <- c("x1", "y", "x2")
+vectors3d(E, lwd=2)
+planes3d(0, 0, 1, 0, col="gray",
+         alpha=0.1)
+# rotate
+V <- matrix(c(1, 1, 1, -1, -1, -1), 2, 3, byrow=TRUE)
+rownames(V) <- c("plus", "minus")
+vectors3d(V, lwd=2, col = c("blue", "red"))
+
+
 # want to use labels that are expressions.
 # text3d() now allows this, using `plotmath3d()` for expressions
 labs <- c(expression(x[1]), "y", expression(x[2]))
 is.expression(labs)
 
 #source(here::here("dev", "vectors3d.R"))
+
+
 
 open3d()
 E <- diag(3)
